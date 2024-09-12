199f773d3993e299a6df04b030dfeb60c0c9919b
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-09-12 10:26:42 +0100

    Verion bump

--- src/testRunner.js
+++ src/testRunner.js
@@ -14,7 +14,7 @@ export const assertNoDuplicates = assertions.assertNoDuplicates;
 let passedTests = 0;
 let failedTests = 0;
 let testResults = [];
-let version = 'v0.0.32';
+let version = 'v0.0.33';
 
 export async function describe(description, callback) {
   console.log(chalk.bold.cyan(`\n${description}`));
