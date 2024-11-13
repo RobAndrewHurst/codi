81eab3b07da5d5b419c4a2840d5b783ffa9df82c
Robert Hurst <robert.hurst@geolytix.co.uk>
2024-11-13 06:34:08 +0200

    v0.0.39 changes

--- src/testRunner.js
+++ src/testRunner.js
@@ -28,7 +28,7 @@ export const assertFalse = assertions.assertFalse;
 export const assertThrows = assertions.assertThrows;
 export const assertNoDuplicates = assertions.assertNoDuplicates;
 
-export const version = 'v0.0.38';
+export const version = 'v0.0.39';
 
 /**
  * CLI entry point for running tests
